7f893b2fca7cec8f3016dbbd5321e50fbd87f799
Anthony Kokkinis <adoni4@gmail.com>
2013-07-09 20:30:27 +0000

    deleted unescessary text

--- web.js
+++ web.js
@@ -5,8 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 	var result = fs.readFileSync('index.html','utf-8', function (err, data) {
 	if (err) throw err;
-var buf = new Buffer(fs.readFileSync('index.html'),'utf-8');
-	return new Buffer(data,'utf-8')
+	return new Buffer(data,'utf-8');
 });
 response.send(result);
 });
