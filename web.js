15174694de1fbe46938fb4a783c705f7398634cd
Anthony Kokkinis <adoni4@gmail.com>
2013-07-09 20:40:19 +0000

    commented out buffer

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 	var result = fs.readFileSync('index.html','utf-8', function (err, data) {
 	if (err) throw err;
-	return new Buffer(data,'utf-8');
+//	return new Buffer(data,'utf-8');
 });
 response.send(result);
 });
