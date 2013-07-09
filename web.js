af3d8049ec9e51cc848855cebd129cc55210d323
Anthony Kokkinis <adoni4@gmail.com>
2013-07-09 20:26:49 +0000

    edited web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+var fs = require('fs');
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+	var result = fs.readFileSync('index.html','utf-8', function (err, data) {
+	if (err) throw err;
+var buf = new Buffer(fs.readFileSync('index.html'),'utf-8');
+	return new Buffer(data,'utf-8')
+});
+response.send(result);
 });
 
 var port = process.env.PORT || 5000;
