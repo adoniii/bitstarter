c591504950f6e4ee71f6d1a5da5e4593e1b2f581
Anthony Kokkinis <adoni4@gmail.com>
2013-07-15 16:06:37 +0000

    updated html index

--- web.js
+++ web.js
@@ -1,16 +1,17 @@
-var fs = require('fs');
+#!/usr/bin/env node
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-	var result = fs.readFileSync('index.html','utf-8', function (err, data) {
-	if (err) throw err;
-//	return new Buffer(data,'utf-8');
-});
-response.send(result);
+	var result = fs.readFileSync('index.html','utf-8');
+	response.send(result.toString());
 });
 
-var port = process.env.PORT || 5000;
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
